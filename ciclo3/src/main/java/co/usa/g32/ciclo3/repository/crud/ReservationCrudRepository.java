db9ee6ce627af505b7c5a101119f5cef0d93f205
joluroba <91926700+joluroba@users.noreply.github.com>
2021-10-25 05:16:11 -0500

    Reto3 Corregido BackEnd
    Corrección de  JsonIngnore, archivo corregido

---
+++ ciclo3/src/main/java/co/usa/g32/ciclo3/repository/crud/ReservationCrudRepository.java
@@ -0,0 +1,10 @@
+package co.usa.g32.ciclo3.repository.crud;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.usa.g32.ciclo3.model.Reservation;
+
+public interface ReservationCrudRepository extends CrudRepository<Reservation, Integer>{
+    
+}
+
